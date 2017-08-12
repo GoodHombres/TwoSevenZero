57e5e98389ac51e7d2e71500b49a21ed782f2b59
esauri <ericksaurisyl@gmail.com>
2017-08-12 19:53:39 -0400

    added android and ios

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # TwoSevenZero
-Race to the White House v2
+Race to the White House
+
+### Commands
+
+* Initialize node modules: ```yarn install```
+* Generate ios and android folders: ```react-native-git-upgrade```
+* Run iOS simulator: ```yarn ios```
+* Run Android Simulator: ```yarn android```
+
+#### Run on device
+* To Run on device:
+ * Edit scripts in package.json
+ * For script name set to your device name (Example: erick)
+ * Set value as react-native run-ios|android 'Erick'
+ * For iOS: Make sure development team is correct
+ * Plug device and run: ```yarn erick```
---
+++ src/containers/Start/styles.js
@@ -0,0 +1,84 @@
+const css = {
+  scroll: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header: {
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#b062ea',
+    width: '100%',
+  },
+  title: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  subtitle: {
+    color: '#e4e4e4',
+    marginTop: 20,
+    fontSize: 24,
+  },
+  content: {
+    alignItems: 'center',
+    maxWidth: 500,
+    width: '90%',
+    margin: 'auto',
+  },
+  instructions: {
+    textAlign: 'left',
+    fontSize: 18,
+    width: '100%',
+    marginTop: 40,
+  },
+  inputContainer: {
+    marginTop: 20,
+    width: '100%',
+  },
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  partyContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+  },
+  democratContainer: {
+    borderColor: '#5480f1',
+    borderWidth: 2,
+    flex: 1,
+  },
+  republicanContainer: {
+    borderColor: '#e74c3c',
+    borderWidth: 2,
+    flex: 1,
+  },
+  democratContainerFill: {
+    backgroundColor: '#5480f1',
+  },
+  republicanContainerFill: {
+    backgroundColor: '#e74c3c',
+  },
+  party: {
+    color: '#fff',
+    fontSize: 24,
+    textAlign: 'center',
+    padding: 10,
+  },
+  democrat: {
+    backgroundColor: '#5480f1',
+  },
+  republican: {
+    backgroundColor: '#e74c3c',
+  },
+  partyDemocrat: { color: '#5480f1' },
+  partyRepublican: { color: '#e74c3c' },
+};
+
+export default css;
